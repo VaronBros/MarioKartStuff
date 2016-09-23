0271012ba3389ffdd5b5bf2ba464ca2e431f5c84
Billy Humphreys <PokeAcer549@outlook.com>
2016-09-23 17:08:30 +0100

    OK I forgot documentation for new index function OK

--- docs/ctgpLeaderboards.md
+++ docs/ctgpLeaderboards.md
@@ -14,3 +14,19 @@ Usage: `get_recent_rkg(num)` where num is between 0-5.
 ### [I]`get_recent_rkg_url`
 This will return the URL for an RKG file for the recent record specified (which is what get_recent_rkg uses)
 Usage: `get_recent_rkg_url(num)` where num is between 0-5.    
+
+### [I]`get_recent_rkg_data`
+This lets you ask for data from the below data list.
+Usage: `get_recent_rkg_data(x, y)` where x is between 0-5 and y is one of the below given data types.
+
+
+## Types of data for `get_recent_rkg_data`
+- href: RKG relative URL
+- finishTime: time (MM:SS.verylongamountofmicroseconds)
+- finishTimeSimple: MM:SS.SSS
+- bestSplit: best split
+- bestSplitSimple: best split (time simplified)
+- hash: SHA1 of file
+- vehicleId: integer of vehicle ID
+- driverId: integer of driver ID
+- dateSet: Time when set (YYYY- MM- DD'T'HH:MM:SS'Z')
--- lib/class.ctgpLeaderboardsIndex.php
+++ lib/class.ctgpLeaderboardsIndex.php
@@ -3,22 +3,27 @@
 // Created by PokeAcer; credit when used.
 
 //Needed for every function:
-$json = file_get_contents('http://tt.chadsoft.co.uk/index.json');
-$jsonarray = json_decode($json, true);
+
 
 function get_recent_rkg($num)
 {
+	$json = file_get_contents('http://tt.chadsoft.co.uk/index.json');
+	$jsonarray = json_decode($json, true);
 	return file_get_contents("http://tt.chadsoft.co.uk" . $jsonarray["recentRecords"][$num]["href"]);
 }
 
 function get_recent_rkg_url($num)
 {
+	$json = file_get_contents('http://tt.chadsoft.co.uk/index.json');
+	$jsonarray = json_decode($json, true);
 	return "http://tt.chadsoft.co.uk" . $jsonarray["recentRecords"][$num]["href"];
 }
 
 function get_recent_rkg_data($num, $type)
 {
-	return "http://tt.chadsoft.co.uk" . $jsonarray["recentRecords"][$num][$type];
+	$json = file_get_contents('http://tt.chadsoft.co.uk/index.json');
+	$jsonarray = json_decode($json, true);
+	return $jsonarray["recentRecords"][$num][$type];
 }
 ?>
 
